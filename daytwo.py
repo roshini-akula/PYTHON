5cb25af0e07e5f9e8073067457fee7e9ca4ddff9
roshini-akula <roshiniakula04@gmail.com>
2025-10-01 18:59:07 +0530

    Create daytwo.py

---
+++ daytwo.py
@@ -0,0 +1,11 @@
+# # for i in range(6):
+# #     print(i)
+# j=0
+# while(j<51):
+#     print(j)
+#     j=j+2
+# a=int(input("enter a number"))
+# if n%2==0:
+#     print("it is an even number")
+# else:
+#         print("it is an odd number")
