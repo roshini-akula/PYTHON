a705c4f1a9461df24b514027af21c6edf4afa128
roshini-akula <roshiniakula04@gmail.com>
2025-10-03 19:07:39 +0530

    Update daythree.py

--- daythree.py
+++ daythree.py
@@ -13,3 +13,21 @@
 # print(r)
 # r.add(12)
 # print(r)
+# def mom(a,b):
+#     print(a+b)
+# a=10
+# b=30
+# mom(a,b)
+# mom(5,6)
+# mom(a,b)
+# mom(5,5)
+# def mom(a,b):
+#     return a+b
+# a=10
+# b=20
+# c=mom(a,b)
+# print(c)
+# d=mom(5,6)
+# print(d)
+# e=mom(7,8)
+# print(e)
