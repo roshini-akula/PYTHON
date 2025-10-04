f37844cc972d1e02a18d011e9b37c6646c35ea98
roshini-akula <roshiniakula04@gmail.com>
2025-10-04 18:25:37 +0530

    Create dayfour.py

---
+++ dayfour.py
@@ -0,0 +1,35 @@
+# list=[1,2,3,4,5,6,7,8,9]
+# print(list[0:9])
+# print(list[2::9])
+# print(list[0::8])
+# def mom(a,b):
+#     print(a+b)
+# a=10
+# b=20
+# c=mom(a,b)
+# print(c)
+#bool
+# if False:
+#     print("valid")
+# else:
+#         print("invalid")
+# try:
+#     a=int(input("enter a number:"))
+#     b=int(input("enter a number:"))
+#     result=a+b
+#     print("sum:",result)
+#  except ValueError:
+#       print("invalid input! please enter numbers only.")
+# #functions
+# def summ(a,b):
+#     return a+b
+# def diff(a,b):
+#     return a-b
+# def prod(a,b):
+#     return a*b
+
+
+# x=2+3j
+# print(x.real)
+# print(x.image)
+# print(type(x))
